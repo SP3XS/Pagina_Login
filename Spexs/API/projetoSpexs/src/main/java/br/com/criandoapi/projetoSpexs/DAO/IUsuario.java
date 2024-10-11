2f72a18095f8e3eeaafae813f6be2a3b2a8c49fd
Gustavo de Oliveira Veroneze <gustavo.oliveroneze@gmail.com>
2024-10-11 13:27:55 -0300

    Delete Spexs directory

--- Spexs/API/projetoSpexs/src/main/java/br/com/criandoapi/projetoSpexs/DAO/IUsuario.java
+++
@@ -1,9 +0,0 @@
-package br.com.criandoapi.projetoSpexs.DAO;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.criandoapi.projetoSpexs.model.Usuario;
-
-public interface IUsuario extends CrudRepository<Usuario, Integer> {
-
-}
